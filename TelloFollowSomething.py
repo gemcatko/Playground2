a604bec099e1668616e67f172d791b27abc90101
gemcatko <gemcatko@gmail.com>
2018-11-16 19:22:28 +0100

    Jast to be safe

--- TelloFollowSomething.py
+++ TelloFollowSomething.py
@@ -1,58 +1,63 @@
 # working version 1
-# author: Arun Ponnusamy
-# website: https://www.arunponnusamy.com
-
-# object detection webcam example
-# usage: python object_detection_webcam.py
-
-# right now YOLOv3 is being used for detecting objects.
-# It's a heavy model to run on CPU. You might see the latency
-# in output frames.
-# To-Do: Add tiny YOLO for real time object detection
-
 # import necessary packages
+from threading import Thread
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
 import cv2
+import simple_takeoff
 
 # variable definition
-#objectToFolow = 'person'
+# objectToFolow = 'person'
 objectToFolow = 'cell phone'
 Xresolution = 680
 Yresolution = 480
 bbox = [[1,1,1,1]]
 percentObjectToFolow = 0.5 # to define how far objectToFolow should be it 0.2 = 20%
-#variable normalization
+# variable normalization
 x1percento = Xresolution/100
 stredX = Xresolution/2
-#boxLengh = bbox[0][2] - bbox[0][0]
-def getNavCoordinates():
-    #X 680*Y 480
-    #[[X1 371,X2 18,Y1 639,Y2 388]]
+
+from time import sleep
+import tellopy
+
+# this is handler for connection to drone
+def handler(event, sender, data, **args):
+    drone = sender
+    if event is drone.EVENT_FLIGHT_DATA:
+        print(data)
+
+
+def getnavcoordinates(bbox, label, conf, frame):
+    # X 680*Y 480
+    # [[X1 371,X2 18,Y1 639,Y2 388]]
     global boxLengh
     global boxMiddleX
     global diffFromMiddleX
     boxLengh = bbox[label.index(objectToFolow)][2] - bbox[label.index(objectToFolow)][0]
-    # boxMiddleX(stred boundig buxu na X ose) = (bboxleght X1 /2) + suradnica X1 <-- kedze suradnic moze byt viac napr: [[272, 294, 440, 480], [-5, 133, 439, 491]] ['remote', 'person'] [0.9549392461776733, 0.6422694325447083] treba volat s indexom label.index(objectToFolow)
+    # boxMiddleX(stred boundig buxu na X ose) = (bboxleght X1 /2) + suradnica X1
+    # <-- kedze suradnic moze byt viac napr:
+    # [[272, 294, 440, 480], [-5, 133, 439, 491]] ['remote', 'person'] [0.9549392461776733, 0.6422694325447083]
+    # treba volat s indexom label.index(objectToFolow)
     boxMiddleX = ((boxLengh/2) + bbox[label.index(objectToFolow)][0])
-    # diffFromMiddleX = abs (stred predpopevedaneho objektu x - stred x podla rozlisenia) # is used for speed of rotation to the object
+    # diffFromMiddleX = abs (stred predpopevedaneho objektu x - stred x podla rozlisenia)
+    #  is used for speed of rotation to the object
     diffFromMiddleX = abs(boxMiddleX - stredX)
     pass
 
-def navigate():
-    #print(label)
-    #print(type(label))
-    #checking if object to folow othervise cannot calculate, to fu
+def navigate(bbox, label, conf, frame):
+    # print(label)
+    # print(type(label))
+    # checking if object to folow othervise cannot calculate, to fu
     if objectToFolow in label:
         label.index(objectToFolow)
         #print("index(objectToFolow:", label.index(objectToFolow) )
-        getNavCoordinates()
+        getnavcoordinates(bbox, label, conf, frame)
         #navigatemiddle()
         navigateForwardBackward()
-        navigateSide()
+        navigateside()
     pass
 
-def navigateWhere():
+def navigatewhere():
     print(type(bbox))
     print("Suradnice vsetky:", bbox)
     print("Suradnica X1:", bbox[0][0])
@@ -61,7 +66,7 @@ def navigateWhere():
     print("Suradnica Y2:", bbox[0][3])
     pass
 
-def navigatemiddle():
+def navigatemiddle(bbox, label, conf, frame):
     if boxMiddleX < stredX:
         if diffFromMiddleX > ( Xresolution * 0.1 ):
             #call funktion to rotate right fast
@@ -83,12 +88,12 @@ def navigatemiddle():
             pass
         pass
     print(stredX,"diffFromMiddleX", diffFromMiddleX, "boxMiddleX", boxMiddleX)
-    #print("boxLengh",boxLengh)
-    #print("boxMiddleX",boxMiddleX)
+    # print("boxLengh",boxLengh)
+    # print("boxMiddleX",boxMiddleX)
     pass
 
 def navigateForwardBackward():
-    #boxLengh = bbox[label.index(objectToFolow)][2] - bbox[label.index(objectToFolow)][0]
+    # boxLengh = bbox[label.index(objectToFolow)][2] - bbox[label.index(objectToFolow)][0]
     if boxLengh < (Xresolution * percentObjectToFolow):
         # go forward
         print("go forward")
@@ -99,7 +104,7 @@ def navigateForwardBackward():
         print ("go backward")
         pass
     pass
-def navigateSide():
+def navigateside():
     if boxMiddleX < stredX:
         if diffFromMiddleX > ( Xresolution * 0.1 ):
             #call funktion to sideway right fast
@@ -120,41 +125,76 @@ def navigateSide():
             pass
         pass
 
-    #call function go sideway right
-    #call function go sideway leftight
+    # call function go sideway right
+    # call function go sideway leftight
     pass
-# open webcam
-webcam = cv2.VideoCapture(0)
 
-if not webcam.isOpened():
-    print("Could not open webcam")
-    exit()
+def getpicturewebcam():
+    # open webcam
+    webcam = cv2.VideoCapture(0)
 
-# loop through frames
-while webcam.isOpened():
-
-    # read frame from webcam
-    status, frame = webcam.read()
-
-    if not status:
-        print("Could not read frame")
+    if not webcam.isOpened():
+        print("Could not open webcam")
         exit()
 
-    # apply object detection
-    bbox, label, conf = cv.detect_common_objects(frame)
-    print(bbox, label, conf)
-    navigate()
+    # loop through frames
+    while webcam.isOpened():
 
-    # draw bounding box over detected objects
-    out = draw_bbox(frame, bbox, label, conf)
+        # read frame from webcam
+        status, frame = webcam.read()
+        print("status:", status)
+        print("frame", type(frame))
 
-    # display output
-    cv2.imshow("Real-time object detection", out)
-    # press "Q" to stop
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        if not status:
+            print("Could not read frame")
+            exit()
 
-# release resources
-webcam.release()
-cv2.destroyAllWindows()
+        # apply object detection
+        bbox, label, conf = cv.detect_common_objects(frame)
+        print(bbox, label, conf)
+        navigate(bbox, label, conf, frame)
+        # draw bounding box over detected objects
+        out = draw_bbox(frame, bbox, label, conf)
+        # display output
+        cv2.imshow("Real-time object detection", out)
+        # press "Q" to stop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        ### This is not right way to go
+        if cv2.waitKey(1) & 0xFF == ord('t'):
+            right()
+
+    # release resources
+    webcam.release()
+    cv2.destroyAllWindows()
+
+def takeoff():
+    drone = tellopy.Tello()
+    try:
+        drone.subscribe(drone.EVENT_FLIGHT_DATA, handler)
+        drone.connect()
+        drone.wait_for_connection(60.0)
+        drone.takeoff()
+        sleep(5)
+        drone.clockwise(100)
+        sleep(5)
+        drone.land()
+        sleep(5)
+    except Exception as ex:
+        print(ex)
+    finally:
+        drone.quit()
+
+
+def main():
+    getpicturewebcam()
+    #simple_takeoff.right()
+
+    #Thread.start(simple_takeoff.right())
+    #Thread.start(getpicturewebcam())
+    #p2.start()
+    #p1.start()
+
+main()
+
 
