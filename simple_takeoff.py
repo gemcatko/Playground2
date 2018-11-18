752bf2a172a5e2dc6f726826400d09305b6ff954
gemcatko <gemcatko@gmail.com>
2018-11-18 13:18:18 +0100

    sve

--- simple_takeoff.py
+++ simple_takeoff.py
@@ -7,23 +7,24 @@ import cv2.cv2 as cv2  # for avoidance of pylint error
 import numpy
 import time
 import cvlib as cv
-#from TelloFollowSomething import getnavcoordinates, navigatewhere,navigatemiddle,navigateForwardBackward
+# from TelloFollowSomething import getnavcoordinates, navigatewhere,navigatemiddle,navigateForwardBackward
 from cvlib.object_detection import draw_bbox
 import logging
 
 # variable definition
 # objectToFolow = 'person'
 objectToFolow = 'sports ball'
-#Xresolution = 680
-#Yresolution = 480
+# Xresolution = 680
+# Yresolution = 480
 Xresolution = 960
 Yresolution = 720
-bbox = [[1,1,1,1]]
-percentObjectToFolow = 0.5 # to define how far objectToFolow should be it 0.2 = 20%
+bbox = [[1, 1, 1, 1]]
+percentObjectToFolow = 0.5  # to define how far objectToFolow should be it 0.2 = 20%
 # variable normalization
-x1percento = Xresolution/100
-stredX = Xresolution/2
+x1percento = Xresolution / 100
+stredX = Xresolution / 2
 drone = tellopy.Tello()
+
 
 def getnavcoordinates(bbox, label):
     # X 680*Y 480
@@ -42,7 +43,7 @@ def getnavcoordinates(bbox, label):
         # <-- kedze suradnic moze byt viac napr:
         # [[272, 294, 440, 480], [-5, 133, 439, 491]] ['remote', 'person'] [0.9549392461776733, 0.6422694325447083]
         # treba volat s indexom label.index(objectToFolow)
-        boxMiddleX = ((boxLengh/2) + bbox[label.index(objectToFolow)][0])
+        boxMiddleX = ((boxLengh / 2) + bbox[label.index(objectToFolow)][0])
         # diffFromMiddleX = abs (stred predpopevedaneho objektu x - stred x podla rozlisenia)
         #  is used for speed of rotation to the object
         diffFromMiddleX = abs(boxMiddleX - stredX)
@@ -53,6 +54,7 @@ def getnavcoordinates(bbox, label):
         pass
     pass
 
+
 def navigatemiddle(bbox, label, conf, frame):
     global boxLengh
     global boxMiddleX
@@ -61,29 +63,38 @@ def navigatemiddle(bbox, label, conf, frame):
         if objectToFolow in label:
             drone.takeoff()
             pass
-        if "knife" in label:
+        if "cell phone" in label:
             drone.land()
             pass
+        if "spoon" in label:
+            drone.clockwise(100)
+            pass
+        if "fork" in label:
+            drone.counter_clockwise(100)
+            pass
+        if ("fork" or "spoon") not in label:
+            drone.counter_clockwise(0)
+            pass
         if boxMiddleX < stredX:
-            if diffFromMiddleX > ( Xresolution * 0.1 ):
-                #call funktion to rotate right fast
+            if diffFromMiddleX > (Xresolution * 0.1):
+                # call funktion to rotate right fast
                 print("doprva rychlo")
                 pass
             else:
-                #print("boxMiddleX", boxMiddleX)
+                # print("boxMiddleX", boxMiddleX)
                 print("doprava")
-                #call funktion to rotate right
+                # call funktion to rotate right
                 pass
             pass
         if boxMiddleX > stredX:
-            if diffFromMiddleX > ( Xresolution * 0.1 ):
+            if diffFromMiddleX > (Xresolution * 0.1):
                 print("lavo rychlo")
-                #call funktion to rotate right fast
-                #drone.counter_clockwise(20)
+                # call funktion to rotate right fast
+                # drone.counter_clockwise(20)
             else:
-                #print("boxMiddleX", boxMiddleX)
+                # print("boxMiddleX", boxMiddleX)
                 print("lavo")
-                #call fuction to rotate dolava
+                # call fuction to rotate dolava
 
                 pass
             pass
@@ -98,10 +109,12 @@ def navigatemiddle(bbox, label, conf, frame):
         print("navigate middle() executeted succesfully")
     pass
 
+
 def handler(event, sender, data, **args):
     drone = sender
     if event is drone.EVENT_FLIGHT_DATA:
         print(data)
+
 
 def test():
     # drone = tellopy.Tello()
@@ -120,6 +133,7 @@ def test():
     finally:
         drone.quit()
 
+
 def right():
     drone = tellopy.Tello()
     try:
@@ -137,8 +151,9 @@ def right():
     finally:
         drone.quit()
 
+
 def video():
-    #drone = tellopy.Tello()
+    # drone = tellopy.Tello()
 
     try:
         drone.connect()
@@ -156,19 +171,19 @@ def video():
                     continue
                 start_time = time.time()
                 image = cv2.cvtColor(numpy.array(frame.to_image()), cv2.COLOR_RGB2BGR)
-                #height, width, channels = image.shape
-                #print("height, width, channels of the picture are:", height, width, channels)
+                # height, width, channels = image.shape
+                # print("height, width, channels of the picture are:", height, width, channels)
 
-                #cv2.imshow('Original', image)
-                #cv2.imshow('Canny', cv2.Canny(image, 100, 200))
+                # cv2.imshow('Original', image)
+                # cv2.imshow('Canny', cv2.Canny(image, 100, 200))
                 # apply object detection
-                #status, frame = webcam.read()
-                #bbox, label, conf = cv.detect_common_objects(image)
-                #faces, confidences = cv.detect_face(image)
-                #print("faces, confidences", faces, confidences)
-                #label = ["person"]
-                #labelos = [[faces]] ['faces'] [confidences]
-                #out = draw_bbox(image,[faces],['person'],[confidences])
+                # status, frame = webcam.read()
+                # bbox, label, conf = cv.detect_common_objects(image)
+                # faces, confidences = cv.detect_face(image)
+                # print("faces, confidences", faces, confidences)
+                # label = ["person"]
+                # labelos = [[faces]] ['faces'] [confidences]
+                # out = draw_bbox(image,[faces],['person'],[confidences])
                 bbox, label, conf = cv.detect_common_objects(image)
                 print(bbox, label, conf)
                 out = draw_bbox(image, bbox, label, conf)
@@ -179,9 +194,9 @@ def video():
                 # press "Q" to stop
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
-                #cv2.imshow("Real-time object detection")
-                #cv2.waitKey(1)
-                frame_skip = int((time.time() - start_time)/frame.time_base)
+                # cv2.imshow("Real-time object detection")
+                # cv2.waitKey(1)
+                frame_skip = int((time.time() - start_time) / frame.time_base)
 
     except Exception as ex:
         exc_type, exc_value, exc_traceback = sys.exc_info()
@@ -191,7 +206,8 @@ def video():
         drone.quit()
         cv2.destroyAllWindows()
 
+
 if __name__ == '__main__':
-    #test()
-    #right()
+    # test()
+    # right()
     video()
